7fc3deafd8457da6c09bd3d547970e0c8aceb545
Janette Gadol <Josh@MacBook-Air.ht.home>
2023-08-03 17:36:40 -0400

    changed file structure

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="" alt=""></img>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
